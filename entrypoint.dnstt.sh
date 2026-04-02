90d69534eacaa88e9e3f5761c5211b3c38868b22
psauxwwf <psauxwwf@d6f.ru>
2026-04-02 22:17:03 +0300

    feat: dnstt docker image

---
+++ entrypoint.dnstt.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+mkdir -p db
+
+if [[ ! -f db/server.key || ! -f db/server.pub ]]; then
+	dnstt-server-linux-amd64 \
+		-gen-key \
+		-privkey-file db/server.key \
+		-pubkey-file db/server.pub
+fi
+
+if [[ $# -eq 0 ]]; then
+	exec dnstt-server-linux-amd64 \
+		-udp "${DNS_PORT:-:53}" \
+		-privkey-file db/server.key \
+		"$NS_QUERY" \
+		"${UPSTREAM:-127.0.0.1:1080}"
+else
+	exec dnstt-server-linux-amd64 "$@"
+fi
